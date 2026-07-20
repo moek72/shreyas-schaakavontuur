1169ccd9d030e080af7a092fb1ea94f152947690
moek72 <57904134+moek72@users.noreply.github.com>
2026-07-20 11:42:34 +0200

    Voeg public/sw.js toe

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'shreya-schaak-v1'
+const BASE_URL = new URL('./', self.location.href).pathname
+const APP_SHELL = [BASE_URL, `${BASE_URL}manifest.webmanifest`, `${BASE_URL}chess-icon.svg`]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => Promise.all(
+      names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name)),
+    )),
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone()
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
+        return response
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match(BASE_URL))),
+  )
+})
